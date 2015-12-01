598e880db73d521cd130c16c8476c84b1ad7fe39
Gareth J Barnard <gjbarnard@gmail.com>
2015-12-01 11:50:21 +0000

    V2.9.1.13 - task #766.

--- lib.php
+++ lib.php
@@ -857,8 +857,8 @@ function theme_campus_render_slide_controls($left) {
     $next = get_string('slidernextslide', 'theme_campus');
     $faleft = 'left';
     $faright = 'right';
-    $prev = '<a class="left carousel-control" data-target="#campusCarousel" data-slide="prev" title="'.$previous.'"><span class="fa fa-chevron-circle-' . $faleft . '"></span></a>';
-    $next = '<a class="right carousel-control" data-target="#campusCarousel" data-slide="next" title="'.$next.'"><span class="fa fa-chevron-circle-' . $faright . '"></span></a>';
+    $prev = '<button class="left carousel-control" data-target="#campusCarousel" data-slide="prev" title="'.$previous.'"><span class="fa fa-chevron-circle-' . $faleft . '"></span></button>';
+    $next = '<button class="right carousel-control" data-target="#campusCarousel" data-slide="next" title="'.$next.'"><span class="fa fa-chevron-circle-' . $faright . '"></span></button>';
 
     return $prev . $next;
 }
