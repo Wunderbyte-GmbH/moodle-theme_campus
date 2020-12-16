a12cc2e42397f5485ca134235dfcf7e48eae3188
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2020-12-16 11:46:57 +0000

    Fix 'Validator: ul within ul not permitted' - #20.

--- layout/tiles/navbar.php
+++ layout/tiles/navbar.php
@@ -40,10 +40,8 @@ if (!empty($hdfancynavbar)) {
             ?>
             <ul class="campusnav nav nav-collapse collapse ml-auto">
                 <?php echo $OUTPUT->custom_menu(); ?>
-                <ul class="nav pull-right">
-                    <li><?php echo $OUTPUT->search_box(); ?></li>
-                    <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
-                </ul>
+                <li><?php echo $OUTPUT->search_box(); ?></li>
+                <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
             </ul>
             <ul class="nav pull-right">
             <?php echo $OUTPUT->gotobottom_menu();
