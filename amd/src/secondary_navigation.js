83a46d4c55994fc8e32ef84014a007a0c111c713
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-09-13 12:49:09 +0100

    Sticky secondary navigation work in progress.

---
+++ amd/src/secondary_navigation.js
@@ -0,0 +1,17 @@
+/* jshint ignore:start */
+define(['jquery', 'core/log'], function($, log) {
+
+    "use strict"; // jshint ;_;
+
+    log.debug('Campus Secondary Navigation AMD');
+
+    return {
+        init: function() {
+            $(document).ready(function($) {
+                log.debug('Campus Secondary Navigation AMD init');
+                var page = document.getElementById("page");
+            });
+        }
+    };
+});
+/* jshint ignore:end */
